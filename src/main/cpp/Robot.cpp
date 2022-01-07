035d22625e62bbe298a77740de1da981fb2c83f1
Modelmat <modelmat@outlook.com.au>
2022-01-07 14:03:59 +1100

    Inversions

--- src/main/cpp/Robot.cpp
+++ src/main/cpp/Robot.cpp
@@ -7,11 +7,27 @@
 #include <fmt/core.h>
 
 #include <frc/smartdashboard/SmartDashboard.h>
+#include <units/math.h>
+#include <wpi/numbers>
 
 void Robot::RobotInit() {
   m_chooser.SetDefaultOption(kAutoNameDefault, kAutoNameDefault);
   m_chooser.AddOption(kAutoNameCustom, kAutoNameCustom);
   frc::SmartDashboard::PutData("Auto Modes", &m_chooser);
+
+  m_motor0.SetInverted(false);
+  m_motor0.SetSensorPhase(false);
+  m_motor1.SetInverted(true);
+  m_motor1.SetSensorPhase(true);
+  m_motor2.SetInverted(true);
+  m_motor2.SetSensorPhase(false);
+
+  m_falcon0.SetInverted(false);
+  m_falcon0.SetSensorPhase(false);
+  m_falcon1.SetInverted(true);
+  m_falcon1.SetSensorPhase(true);
+  m_falcon2.SetInverted(true);
+  m_falcon2.SetSensorPhase(false);
 }
 
 /**
@@ -22,7 +38,14 @@ void Robot::RobotInit() {
  * <p> This runs after the mode specific periodic functions, but before
  * LiveWindow and SmartDashboard integrated updating.
  */
-void Robot::RobotPeriodic() {}
+void Robot::RobotPeriodic() {
+  frc::SmartDashboard::PutNumber("SRX Normal Vel", m_motor0.GetSelectedSensorVelocity());
+  frc::SmartDashboard::PutNumber("SRX Inverted Vel", m_motor1.GetSelectedSensorVelocity());
+  frc::SmartDashboard::PutNumber("SRX No SensorPhase Inverted Vel", m_motor2.GetSelectedSensorVelocity());
+  frc::SmartDashboard::PutNumber("Falcon Normal Vel", m_falcon0.GetSelectedSensorVelocity());
+  frc::SmartDashboard::PutNumber("Falcon Inverted Vel", m_falcon1.GetSelectedSensorVelocity());
+  frc::SmartDashboard::PutNumber("Falcon No SensorPhase Inverted Vel", m_falcon2.GetSelectedSensorVelocity());
+}
 
 /**
  * This autonomous (along with the chooser code above) shows how to select
@@ -58,7 +81,15 @@ void Robot::AutonomousPeriodic() {
 
 void Robot::TeleopInit() {}
 
-void Robot::TeleopPeriodic() {}
+void Robot::TeleopPeriodic() {
+  // Just run a periodic sine function
+  m_motor0.Set(-1);
+  m_motor1.Set(-1);
+  m_motor2.Set(-1);
+  m_falcon0.Set(-1);
+  m_falcon1.Set(-1);
+  m_falcon2.Set(-1);
+}
 
 void Robot::DisabledInit() {}
 
@@ -67,6 +98,38 @@ void Robot::DisabledPeriodic() {}
 void Robot::TestInit() {}
 
 void Robot::TestPeriodic() {}
+
+void Robot::SimulationInit() {}
+
+void Robot::SimulationPeriodic() {
+  m_driveSim1.SetInputs(m_motor0.Get() * 12_V, m_motor0.Get() * 12_V);
+  m_driveSim2.SetInputs(m_motor1.Get() * 12_V, m_motor1.Get() * 12_V);
+  m_driveSim3.SetInputs(m_motor2.Get() * 12_V, m_motor2.Get() * 12_V);
+  m_driveSim4.SetInputs(m_falcon0.Get() * 12_V, m_falcon0.Get() * 12_V);
+  m_driveSim5.SetInputs(m_falcon1.Get() * 12_V, m_falcon1.Get() * 12_V);
+  m_driveSim6.SetInputs(m_falcon2.Get() * 12_V, m_falcon2.Get() * 12_V);
+
+  m_driveSim1.Update(20_ms);
+  m_driveSim2.Update(20_ms);
+  m_driveSim3.Update(20_ms);
+  m_driveSim4.Update(20_ms);
+  m_driveSim5.Update(20_ms);
+  m_driveSim6.Update(20_ms);
+
+  m_motorSim0.SetQuadratureRawPosition(m_driveSim1.GetLeftPosition().value());
+  m_motorSim1.SetQuadratureRawPosition(m_driveSim2.GetLeftPosition().value());
+  m_motorSim2.SetQuadratureRawPosition(m_driveSim3.GetLeftPosition().value());
+  m_motorSim0.SetQuadratureVelocity(m_driveSim1.GetLeftVelocity().value());
+  m_motorSim1.SetQuadratureVelocity(m_driveSim2.GetLeftVelocity().value());
+  m_motorSim2.SetQuadratureVelocity(m_driveSim3.GetLeftVelocity().value());
+
+  m_falconSim0.SetIntegratedSensorRawPosition(m_driveSim4.GetLeftPosition().value());
+  m_falconSim1.SetIntegratedSensorRawPosition(m_driveSim5.GetLeftPosition().value());
+  m_falconSim2.SetIntegratedSensorRawPosition(m_driveSim6.GetLeftPosition().value());
+  m_falconSim0.SetIntegratedSensorVelocity(m_driveSim4.GetLeftVelocity().value());
+  m_falconSim1.SetIntegratedSensorVelocity(m_driveSim5.GetLeftVelocity().value());
+  m_falconSim2.SetIntegratedSensorVelocity(m_driveSim6.GetLeftVelocity().value());
+}
 
 #ifndef RUNNING_FRC_TESTS
 int main() {
--- src/main/include/Robot.h
+++ src/main/include/Robot.h
@@ -8,6 +8,12 @@
 
 #include <frc/TimedRobot.h>
 #include <frc/smartdashboard/SendableChooser.h>
+#include <ctre/phoenix/motorcontrol/can/WPI_TalonSRX.h>
+#include <ctre/phoenix/motorcontrol/can/WPI_TalonFX.h>
+#include <ctre/phoenix/motorcontrol/TalonSRXSimCollection.h>
+#include <ctre/phoenix/motorcontrol/TalonFXSimCollection.h>
+#include <frc/simulation/DifferentialDrivetrainSim.h>
+#include <frc/Timer.h>
 
 class Robot : public frc::TimedRobot {
  public:
@@ -21,10 +27,64 @@ class Robot : public frc::TimedRobot {
   void DisabledPeriodic() override;
   void TestInit() override;
   void TestPeriodic() override;
+  void SimulationInit() override;
+  void SimulationPeriodic() override;
 
  private:
   frc::SendableChooser<std::string> m_chooser;
   const std::string kAutoNameDefault = "Default";
   const std::string kAutoNameCustom = "My Auto";
   std::string m_autoSelected;
+  
+  ctre::phoenix::motorcontrol::can::WPI_TalonSRX m_motor0{0};
+  ctre::phoenix::motorcontrol::can::WPI_TalonSRX m_motor1{1};
+  ctre::phoenix::motorcontrol::can::WPI_TalonSRX m_motor2{2};
+  ctre::phoenix::motorcontrol::can::WPI_TalonFX m_falcon0{0};
+  ctre::phoenix::motorcontrol::can::WPI_TalonFX m_falcon1{1};
+  ctre::phoenix::motorcontrol::can::WPI_TalonFX m_falcon2{2};
+
+  ctre::phoenix::motorcontrol::TalonSRXSimCollection m_motorSim0{m_motor0};
+  ctre::phoenix::motorcontrol::TalonSRXSimCollection m_motorSim1{m_motor1};
+  ctre::phoenix::motorcontrol::TalonSRXSimCollection m_motorSim2{m_motor2};
+  ctre::phoenix::motorcontrol::TalonFXSimCollection m_falconSim0{m_falcon0};
+  ctre::phoenix::motorcontrol::TalonFXSimCollection m_falconSim1{m_falcon1};
+  ctre::phoenix::motorcontrol::TalonFXSimCollection m_falconSim2{m_falcon2};
+
+
+  frc::sim::DifferentialDrivetrainSim m_driveSim1 =
+    frc::sim::DifferentialDrivetrainSim::CreateKitbotSim(
+      frc::sim::DifferentialDrivetrainSim::KitbotMotor::DualCIMPerSide, // 2 CIMs per side.
+      frc::sim::DifferentialDrivetrainSim::KitbotGearing::k10p71,       // 10.71:1
+      frc::sim::DifferentialDrivetrainSim::KitbotWheelSize::kSixInch    // 6" diameter wheels.
+  );
+  frc::sim::DifferentialDrivetrainSim m_driveSim2 =
+    frc::sim::DifferentialDrivetrainSim::CreateKitbotSim(
+      frc::sim::DifferentialDrivetrainSim::KitbotMotor::DualCIMPerSide, // 2 CIMs per side.
+      frc::sim::DifferentialDrivetrainSim::KitbotGearing::k10p71,       // 10.71:1
+      frc::sim::DifferentialDrivetrainSim::KitbotWheelSize::kSixInch    // 6" diameter wheels.
+  );
+  frc::sim::DifferentialDrivetrainSim m_driveSim3 =
+    frc::sim::DifferentialDrivetrainSim::CreateKitbotSim(
+      frc::sim::DifferentialDrivetrainSim::KitbotMotor::DualCIMPerSide, // 2 CIMs per side.
+      frc::sim::DifferentialDrivetrainSim::KitbotGearing::k10p71,       // 10.71:1
+      frc::sim::DifferentialDrivetrainSim::KitbotWheelSize::kSixInch    // 6" diameter wheels.
+  );
+  frc::sim::DifferentialDrivetrainSim m_driveSim4 =
+    frc::sim::DifferentialDrivetrainSim::CreateKitbotSim(
+      frc::sim::DifferentialDrivetrainSim::KitbotMotor::DualCIMPerSide, // 2 CIMs per side.
+      frc::sim::DifferentialDrivetrainSim::KitbotGearing::k10p71,       // 10.71:1
+      frc::sim::DifferentialDrivetrainSim::KitbotWheelSize::kSixInch    // 6" diameter wheels.
+  );
+  frc::sim::DifferentialDrivetrainSim m_driveSim5 =
+    frc::sim::DifferentialDrivetrainSim::CreateKitbotSim(
+      frc::sim::DifferentialDrivetrainSim::KitbotMotor::DualCIMPerSide, // 2 CIMs per side.
+      frc::sim::DifferentialDrivetrainSim::KitbotGearing::k10p71,       // 10.71:1
+      frc::sim::DifferentialDrivetrainSim::KitbotWheelSize::kSixInch    // 6" diameter wheels.
+  );
+  frc::sim::DifferentialDrivetrainSim m_driveSim6 =
+    frc::sim::DifferentialDrivetrainSim::CreateKitbotSim(
+      frc::sim::DifferentialDrivetrainSim::KitbotMotor::DualCIMPerSide, // 2 CIMs per side.
+      frc::sim::DifferentialDrivetrainSim::KitbotGearing::k10p71,       // 10.71:1
+      frc::sim::DifferentialDrivetrainSim::KitbotWheelSize::kSixInch    // 6" diameter wheels.
+  );
 };
